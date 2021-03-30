b230bab8d3c236f995c4cd978a16ffd7280688b9
Mahesh <mahesh199942@gmail.com>
2021-03-30 19:08:13 +0530

    edited

--- if_else.py
+++ if_else.py
@@ -1,2 +1,8 @@
-a = input("enter User Name: ")
-print(a)
+UserName = input("enter User Name: ")
+print(UserName)
+password = input("Password: ")
+print(password)
+if password == "mahesh":
+    print("sucessfull")
+else:
+    print("incurrent")
